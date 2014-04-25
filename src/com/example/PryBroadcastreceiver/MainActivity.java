1b24c6a6a6b1723d56fbc157556378fe76fc2399
Henry AT <yiperu@gmail.com>
2014-04-24 22:04:46 -0300

    Implementamos las configuraciones iniciales de aun app que pueda acceder a internet, ahora solo mostrando en el LogCat el source code de google

--- src/com/example/PryBroadcastreceiver/MainActivity.java
+++ src/com/example/PryBroadcastreceiver/MainActivity.java
@@ -1,7 +1,20 @@
 package com.example.PryBroadcastreceiver;
 
 import android.app.Activity;
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.StrictMode;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class MainActivity extends Activity {
     /**
@@ -11,5 +24,82 @@ public class MainActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+
+        System.out.println("==================  INICIO  ==================");
+        try {
+            URL myurl = new URL("http://www.google.com");
+            HttpURLConnection con = (HttpURLConnection)myurl.openConnection();
+            readStream(con.getInputStream());
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println("================== FIN ======================");
+
     }
+
+    public boolean isNetworkAvailable(){
+        ConnectivityManager cm = (ConnectivityManager)getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = cm.getActiveNetworkInfo();
+        if (networkInfo != null && networkInfo.isConnected()) {
+            return true;
+        }
+        return false;
+    }
+    public boolean isWifiConnection(){
+        ConnectivityManager cm = (ConnectivityManager)getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = cm.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
+        boolean isWifi = networkInfo.isConnected();
+        return isWifi;
+    }
+
+    public boolean isMobileConnection(){
+        ConnectivityManager cm = (ConnectivityManager)getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = cm.getNetworkInfo(ConnectivityManager.TYPE_MOBILE);
+        boolean isMobile = networkInfo.isConnected();
+        return isMobile;
+    }
+
+    private void readStream(InputStream in){
+        BufferedReader reader = null;
+        try {
+            reader = new BufferedReader(new InputStreamReader(in));
+            String line = "";
+            while((line = reader.readLine()) != null){
+                System.out.println(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {}
+        if (reader != null) {
+            try {
+                reader.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private class DownloadURL extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... params) {
+            // TODO Auto-generated method stub
+            return null;
+        }
+        @Override
+        protected void onPostExecute(String result) {
+            // TODO Auto-generated method stub
+            super.onPostExecute(result);
+        }
+        @Override
+        protected void onPreExecute() {
+            // TODO Auto-generated method stub
+            super.onPreExecute();
+        }
+
+
 }
